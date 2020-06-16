556ddd5881fb1614a191e8bcd5d8aca4bacde970
Guillaume Desforges <aceus02@gmail.com>
2020-06-16 17:26:28 +0200

    Edit funflow2 test

--- funflow2/TestFunflow.hs
+++ funflow2/TestFunflow.hs
@@ -7,13 +7,19 @@ import Funflow (Flow, caching, ioFlow, pureFlow, runFlow)
 
 main :: IO ()
 main = do
+  -- Run multiple flows to test funflow's capabilities
   putStr "\n------ TESTING ------\n"
-  -- Run a simple flow from a pure function
-  testFlow @Int @Int "a flow from a pure function" (pureFlow (+ 1)) 0
+  let somePureFlow :: Flow Int Int
+      somePureFlow = pureFlow (+ 1)
+  testFlow @Int @Int "a flow from a pure function" somePureFlow 0
   putStr "\n---------------------\n"
-  testFlow @() @() "a flow with IO" (ioFlow (const $ putStrLn "Some IO operation")) ()
+  let someIoFlow :: Flow () ()
+      someIoFlow = ioFlow $ const $ putStrLn "Some IO operation"
+  testFlow @() @() "a flow with IO" someIoFlow ()
   putStr "\n---------------------\n"
-  testFlow @Int @Int "a flow from a pure function with caching" (caching "increment" $ pureFlow (+ 1)) 0
+  let someCachedFlow :: Flow Int Int
+      someCachedFlow = caching "increment" $ pureFlow (+ 1)
+  testFlow @Int @Int "a flow from a pure function with caching" someCachedFlow 0
   putStr "\n------  DONE   ------\n"
 
 testFlow :: forall i o. (Show i, Show o) => String -> Flow i o -> i -> IO ()
