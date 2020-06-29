096d59c46030d66baf7ec24c53a915b2534c2333
Guillaume Desforges <aceus02@gmail.com>
2020-06-29 17:16:53 +0200

    Setup haskell.nix properly and set github action tests

--- nix/default.nix
+++ nix/default.nix
@@ -1,12 +1,23 @@
-let
-  iohk-overlay = import ./haskell.nix-src.nix;
-in
-{ pkgs ? import ./nixpkgs-src.nix iohk-overlay
+{ # Fetch the latest haskell.nix and import its default.nix
+  haskellNix ? import ./haskell.nix-src.nix {}
+
+  # haskell.nix provides access to the nixpkgs pins which are used by its CI,
+  # hence you will be more likely to get cache hits when using these.
+  # But you can also just use your own, e.g. '<nixpkgs>'.
+, nixpkgsSrc ? haskellNix.sources.nixpkgs-2003
+
+  # haskell.nix provides some arguments to be passed to nixpkgs, including some
+  # patches and also the haskell.nix functionality itself as an overlay.
+, nixpkgsArgs ? haskellNix.nixpkgsArgs
+
+  # import nixpkgs with overlays
+, pkgs ? import nixpkgsSrc nixpkgsArgs
 }:
-  pkgs.haskell-nix.stackProject {
-    src = pkgs.haskell-nix.haskellLib.cleanGit { src = ./..; };
-    modules = [
-        # The SQLite tests break due to not finding 'echo'. We disable them for now.
-        {packages.funflow.components.tests.unit-tests.testFlags = ["-p '! /SQLite/'"];}
-    ];
-  }
+
+pkgs.haskell-nix.stackProject {
+  # 'cleanGit' cleans a source directory based on the files known by git
+  src = pkgs.haskell-nix.haskellLib.cleanGit {
+    name = "funflow2-project";
+    src = ./..;
+  };
+}
