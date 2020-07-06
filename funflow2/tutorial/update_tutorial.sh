92dcf041c29a9db2cc759ebb893235d3093cfac7
Guillaume Desforges <aceus02@gmail.com>
2020-07-06 11:13:44 +0200

    Readme for tutorial

--- funflow2/tutorial/update_tutorial.sh
+++ funflow2/tutorial/update_tutorial.sh
@@ -3,5 +3,5 @@
 
 stack build funflow2-tutorial \
     && echo "Generating html..." \
-    && stack --verbosity silent run funflow2-tutorial:exe:tutorial1 > funflow2/tutorial/out/tutorial1.html \
+    && stack --verbosity silent run funflow2-tutorial:exe:tutorial1 > out/tutorial1.html \
     && echo "done"
