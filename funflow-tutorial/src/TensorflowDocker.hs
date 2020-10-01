d03cf010d57a770fa59ebf5343283fcc880704fa
dorranh <dorran.howell@tweag.io>
2020-10-01 18:01:34 +0200

    Update docker tutorial

--- funflow-tutorial/src/TensorflowDocker.hs
+++ funflow-tutorial/src/TensorflowDocker.hs
@@ -22,8 +22,11 @@ import System.Directory.Funflow (moveDirectoryContent)
 import System.IO (FilePath)
 
 -- | Helper for getting the absolute path to the tensorflow example directory
-tfExampleDir :: IO (Path Abs Dir)
-tfExampleDir = flip (</>) [reldir|./tensorflow-example|] =<< parseAbsDir =<< getCurrentDirectory
+tfExampleDir :: () -> IO (Path Abs Dir)
+tfExampleDir _ = do
+  cwd <- getCurrentDirectory
+  cwdAbs <- parseAbsDir cwd
+  return $ cwdAbs </> [reldir|./tensorflow-example|]
 
 --------------------------------------------------------------------------------------------
 ------------------------------------- FLOWS ------------------------------------------------
@@ -33,38 +36,32 @@ tfExampleDir = flip (</>) [reldir|./tensorflow-example|] =<< parseAbsDir =<< get
 copyExampleToStore :: Flow () CS.Item
 copyExampleToStore = proc _ -> do
   exampleDir <- ioFlow tfExampleDir -< ()
-  exampleItem <- putDir -< exampleDir
+  putDir -< exampleDir
+
+trainTaskConfig :: DockerTaskConfig
+trainTaskConfig =
+  DockerTaskConfig
+    { DE.image = "tensorflow/tensorflow:2.3.0",
+      DE.command = "bash",
+      -- Note: we are installing our script's requirements so we can use the default tensorflow image
+      -- we could have also prepared an image with these requirements already installed.
+      DE.args =
+        [ "-c",
+          "pip install -r /tensorflow-example/requirements.txt && \
+          \ python /tensorflow-example/train_mnist.py --n_epochs 10 ./model"
+        ]
+    }
 
 -- | Trains a tensorflow digit recognition model and outputs the serialized model
 trainModel :: Flow CS.Item CS.Item
 trainModel = proc exampleItem -> do
   -- Define a volume for the example directory
   let exampleVolume = VolumeBinding {DE.item = exampleItem, DE.mount = [absdir|/tensorflow-example/|]}
-  modelItem <-
-    dockerFlow
-      ( DockerTaskConfig
-          { DE.image = "tensorflow/tensorflow:2.3.0",
-            DE.command = "bash",
-            -- Note: we are installing our script's requirements so we can use the default tensorflow image
-            -- we could have also prepared an image with these requirements already installed.
-            DE.args =
-              [ "-c",
-                "pip install -r /tensorflow-example/requirements.txt && \
-                \ python /tensorflow-example/train-mnist.py --n_epochs 100 ."
-              ]
-          }
-      )
-      -<
-        DockerTaskInput {DE.inputBindings = [exampleVolume], DE.argsVals = mempty}
-  -- DockerTask outputs are written to the content store. We can get their path with getDir
-  -- and print the path before returning the content store item for downstream tasks.
-  modelPath <- getDir -< modelItem
-  ioFlow print -< ("Training complete. The trained model was saved at: " ++ show modelPath)
-  return -< modelItem
+  (dockerFlow trainTaskConfig) -< DockerTaskInput {DE.inputBindings = [exampleVolume], DE.argsVals = mempty}
 
 -- | Runs a trained tensorflow digit recognition model on some demo images and outputs a plot
 -- of its classifications.
-runModel :: Flow (CS.Item, CS.Item) CS.Item
+runModel :: Flow (CS.Item, CS.Item) ()
 runModel = proc (exampleItem, trainedModelItem) -> do
   -- Define volumes for the example directory and the previously trained model
   let exampleVolume = VolumeBinding {DE.item = exampleItem, DE.mount = [absdir|/tensorflow-example/|]}
@@ -77,21 +74,20 @@ runModel = proc (exampleItem, trainedModelItem) -> do
             DE.args =
               [ "-c",
                 "pip install -r /tensorflow-example/requirements.txt && \
-                \ python /tensorflow-example/inference-mnist.py /trained/model /tensorflow-example/demo-images ./summary.png"
+                \ python /tensorflow-example/inference_mnist.py /trained/model /tensorflow-example/demo-images ./summary.png"
               ]
           }
       )
       -<
         DockerTaskInput {DE.inputBindings = [exampleVolume, trainedModelVolume], DE.argsVals = mempty}
-  modelPath <- getDir -< modelItem
-  ioFlow print -< ("Model inferencing complete. The model outputs were saved at: " ++ show modelPath)
-  return -< modelItem
+  modelSummaryPath <- getDir -< modelSummaryItem
+  ioFlow print -< ("Model inferencing complete. The model outputs were saved at: " ++ show modelSummaryPath)
 
 -- Now we can plug everything into the final Flow DAG
 mainFlow :: Flow () ()
 mainFlow = proc _ -> do
   tensorflowExample <- copyExampleToStore -< ()
   trainedModel <- trainModel -< tensorflowExample
-  modelResult <- runModel -< (tensorflowExample, trainedModel)
+  runModel -< (tensorflowExample, trainedModel)
 
 main = runFlow mainFlow () :: IO ()
