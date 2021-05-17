49086d3c46283589353a492ccb1e78816a5331fa
dorranh <dorran.howell@tweag.io>
2021-05-17 16:03:32 +0200

    [ci skip] [WIP] Add funflow1 aliases. Does not build.

--- funflow/src/Funflow/Compat.hs
+++ funflow/src/Funflow/Compat.hs
@@ -1,11 +1,23 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE OverloadedLabels #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE FlexibleContexts #-}
 module Funflow.Compat where
 
-import Funflow (caching)
+import Funflow (caching', caching)
 import Funflow.Flow (dockerFlow, getDirFlow, ioFlow, pureFlow, putDirFlow)
 
 stepIO = ioFlow
 
-stepIO' = flip caching ioFlow
+stepIO' = (flip caching) ioFlow
 
 docker = dockerFlow
 
