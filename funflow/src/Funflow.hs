f08b3fc68ece199c82e28d1ca16781af5d04028c
dorranh <dorran.howell@tweag.io>
2021-06-14 14:51:32 +0200

    Clean up aliases

--- funflow/src/Funflow.hs
+++ funflow/src/Funflow.hs
@@ -7,14 +7,13 @@ module Funflow
     module Funflow.Run,
     (>>>),
     caching,
-    caching',
     tryE,
     throwE,
   )
 where
 
 import Control.Arrow ((>>>))
-import Control.Kernmantle.Caching (caching, caching')
+import Control.Kernmantle.Caching (caching)
 import Control.Kernmantle.Error (throwE, tryE)
 import Funflow.Flow
 import Funflow.Run
